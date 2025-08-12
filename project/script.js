e8d4f5c133854d7c1fd470042482bbdb2ae49431
Devang Bhuvad <devangbhuvad2004@gmail.com>
2025-08-12 17:03:50 +0530

    hii

--- project/script.js
+++ project/script.js
@@ -1,17 +1,17 @@
 function temperature(){
     var c = document.getElementById("celsius").value;
-    var f = (c * 9/5) + 32
-    document.getElementById("fahrenheit").value = f
+    var f = (c * 9/5) + 32;
+    document.getElementById("fahrenheit").value = f;
 }
 
 function weight(){
-    var kg = document.getElementById("kilograms").value;
-    var p = kg * 2.20462
-    document.getElementById("pounds").value = p
+    var kg = document.getElementById("kilo").value;
+    var p = kg * 2.20462;
+    document.getElementById("pounds").value = p;
 }
 
 function distance(){
-    var km = document.getElementById("kilometers").value;
-    var m = km * 0.621371
-    document.getElementById("miles").value = m
+    var km = document.getElementById("km").value;
+    var m = km * 0.621371;
+    document.getElementById("miles").value = m;
 }
